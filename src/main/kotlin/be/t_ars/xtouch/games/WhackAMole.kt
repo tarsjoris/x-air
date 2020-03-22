a374c496e7fd9dbe6ba643226d5023c940683c22
tjoris <tarsjoris@hotmail.com>
2020-03-22 20:15:16 +0100

    Count score and indicate error Whack a Mole

--- src/main/kotlin/be/t_ars/xtouch/games/WhackAMole.kt
+++ src/main/kotlin/be/t_ars/xtouch/games/WhackAMole.kt
@@ -5,6 +5,7 @@ import be.t_ars.xtouch.xctl.IXctlConnection
 import be.t_ars.xtouch.xctl.IXctlConnectionListener
 import be.t_ars.xtouch.xctl.IXctlOutput
 import be.t_ars.xtouch.xctl.XctlConnectionImpl
+import kotlinx.coroutines.delay
 import kotlin.random.Random
 
 private class Listener(private val output: IXctlOutput) : IXctlConnectionListener, IXTouchListener {
@@ -12,107 +13,143 @@ private class Listener(private val output: IXctlOutput) : IXctlConnectionListene
 	private val channelButtons = IXctlOutput.EChannelButton.values()
 	private val buttons = IXctlOutput.EButton.values()
 
+	private var score = 0
 	private var currentChannels = false
-	private var currentChannel = 0
+	private var currentChannel = 1
 	private var currentChannelButton = IXctlOutput.EChannelButton.SELECT
 	private var currentButton = IXctlOutput.EButton.ENCODER_TRACK
 
-	override fun connected() =
+	override fun connected() {
+		score = 0
+		clearCurrent()
 		placeMole()
+	}
 
 	override suspend fun channelRecPressed(channel: Int) {
 		if (currentChannels && currentChannel == channel && currentChannelButton == IXctlOutput.EChannelButton.REC) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun channelSoloPressed(channel: Int) {
 		if (currentChannels && currentChannel == channel && currentChannelButton == IXctlOutput.EChannelButton.SOLO) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun channelMutePressed(channel: Int) {
 		if (currentChannels && currentChannel == channel && currentChannelButton == IXctlOutput.EChannelButton.MUTE) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun channelSelectPressed(channel: Int) {
 		if (currentChannels && currentChannel == channel && currentChannelButton == IXctlOutput.EChannelButton.SELECT) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun encoderTrackPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.ENCODER_TRACK) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun encoderSendPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.ENCODER_SEND) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun encoderPanPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.ENCODER_PAN) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun encoderPluginPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.ENCODER_PLUGIN) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun encoderEqPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.ENCODER_EQ) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun encoderInstPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.ENCODER_INST) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun previousBankPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.PREV_BANK) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun nextBankPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.NEXT_BANK) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun previousChannelPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.PREV_CHANNEL) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun nextChannelPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.NEXT_CHANNEL) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun flipPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.FLIP) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun globalViewPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.GLOBAL_VIEW) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
@@ -129,54 +166,72 @@ private class Listener(private val output: IXctlOutput) : IXctlConnectionListene
 			}
 		) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun midiTracksPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.MIDI_TRACKS) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun inputsPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.INPUTS) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun audioTracksPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.AUDIO_TRACKS) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun audioInstPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.AUDIO_INST) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun auxPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.AUX) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun busesPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.BUSES) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun outputsPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.OUTPUTS) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
 	override suspend fun userPressed() {
 		if (!currentChannels && currentButton == IXctlOutput.EButton.USER) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
@@ -189,6 +244,8 @@ private class Listener(private val output: IXctlOutput) : IXctlConnectionListene
 			}
 		) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
@@ -203,6 +260,8 @@ private class Listener(private val output: IXctlOutput) : IXctlConnectionListene
 			}
 		) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
@@ -215,10 +274,32 @@ private class Listener(private val output: IXctlOutput) : IXctlConnectionListene
 			}
 		) {
 			next()
+		} else {
+			loose()
 		}
 	}
 
+	private suspend fun loose() {
+		output.setScribbleTrip(1, IXctlOutput.EScribbleColor.RED, true, " GAME", " OVER")
+		for (channel in 2..8) {
+			output.setScribbleTrip(channel, IXctlOutput.EScribbleColor.RED, false, "", "")
+		}
+		delay(1000)
+		for (channel in 1..8) {
+			output.setScribbleTrip(channel, IXctlOutput.EScribbleColor.WHITE, false, "", "")
+		}
+		score = 0
+		clearCurrent()
+		placeMole()
+	}
+
 	private fun next() {
+		clearCurrent()
+		++score
+		placeMole()
+	}
+
+	private fun clearCurrent() {
 		if (currentChannels) {
 			output.setChannelButtonLED(
 				currentChannel,
@@ -231,10 +312,10 @@ private class Listener(private val output: IXctlOutput) : IXctlConnectionListene
 				IXctlOutput.ELEDMode.OFF
 			)
 		}
-		placeMole()
 	}
 
 	private fun placeMole() {
+		output.setDigits(score)
 		currentChannels = random.nextBoolean()
 		if (currentChannels) {
 			currentChannel = random.nextInt(8) + 1
@@ -245,7 +326,7 @@ private class Listener(private val output: IXctlOutput) : IXctlConnectionListene
 				IXctlOutput.ELEDMode.ON
 			)
 		} else {
-			currentButton = buttons[random.nextInt(buttons.size)]
+			currentButton = buttons[random.nextInt(buttons.size - 1)]
 			output.setButtonLED(
 				currentButton,
 				IXctlOutput.ELEDMode.ON
